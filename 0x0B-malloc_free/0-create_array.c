4a7c6462434e98dad326ae1db7e94894764d562f
hadeel <hadeelmoh512@gmail.com>
2023-10-04 13:37:18 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	str = malloc(size * sizeof(char));
-	if (size == 0 || size == NULL)
+	if (size == 0 || str == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
 		str[i] = c;
